befc57c21bb80f7801751cfb2976b4dc75cc6572
kdev0x <119963213+kdev0x@users.noreply.github.com>
2023-03-01 19:43:18 +0300

    Update 008-introduction-to-printf.c

--- 01-introduction-to-data/008-introduction-to-printf.c
+++ 01-introduction-to-data/008-introduction-to-printf.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
 int main(){
-    unsigned char v1 = 65; //01000001
-    unsigned char v2 = 'A';
+    printf("Welcome to C programming language\n");
+    printf("welcome to assembly programming language\n");
 
-    //unsigned char v3 = 0b00000110;
-    //v2 = v2 | v3;
+    printf("Welcome to %s\n", "Python");
+    printf("Welcome to %s\n", "C++");
 
-    //v2 = v2 ^ v2;
-    //v2 = v2 | 0b01000111;
-    unsigned char v3 = 0b00000001;
-    v2 = v2 | v3;
-    v3 = v3 << 1;
-    v2 = v2 | v3;
-    v3 = v3 << 1 ;
-    v2 = v2 | v3;
+    printf("Welcome to %s and %s\n", "Java", "PHP");
+    printf("welcome to %s and %s and %s\n", "html" , "css", "java script");
 
-    printf("%c\n", v2);
+    /* Welcome to Machine, Assembly, and C */
+    printf("Welcome to Machine, Assembly, and C\n");
+    
+    printf("Welcome to %s, %s, and %s\n", "Machine", "Assembly", "C");
+    
+    printf("Welcome to Machine, Assembly, and %s\n", "C");
+    printf("Welcome to Machine, %s, and C\n", "Assembly");
+    printf("Welcome to %s, Assembly, and C/n", "Machine");
 
+    printf("Welcome to %s, %s, and C\n", "Assembly", "Machine");
+    printf("Welcome to %s, Assembly, and %s\n", "Machine", "C");
+    printf("Welcome to Machine, %s, and %s\n", "Assembly", "C");
+
+    printf("%celcome\n", 'W');
+    printf("Welcome to %c and %cH%c\n", 'C', 'P', 'P');
+
+    printf("%c%c%c%c%c%c\n", 'P','y','t','h','o','n');
+
+    printf("Welcome to %cython, %cython, and %cython\n", 'P', 'C', 'J');
+    /* %s %c */
+    printf("%s%s\n", "Kha","lid");
+    printf("K%s%s\n", "hal", "id");
+    printf("Kh%s%sid\n", "a", "l");
     return 0;
 }
